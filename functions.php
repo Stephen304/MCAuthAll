4bfd31a960d6c10b59ef5d88109efe6cd3329797
Baltazar Sirgue <BaltazarSirgue@users.noreply.github.com>
2015-02-23 15:38:04 +0100

    Login() using cURL()

--- functions.php
+++ functions.php
@@ -9,11 +9,7 @@ function cURL($url) {
 }
 function login($user, $password) {
 	$url = 'http://login.minecraft.net/?user='.$user.'&password='.$password.'&version=12';
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	return curl_exec($ch);
+	return cURL($ch);
 }
 function randSess() {
 	$i = 0;
